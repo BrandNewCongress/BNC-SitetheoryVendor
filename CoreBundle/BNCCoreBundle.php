be31e6d9adae4360f95f0b1573e2b6e2264db49b
chadwickmeyer <chadwick@gutensite.com>
2017-03-06 15:48:39 -0800

    consolidate full curated bundle which will be a submodule

---
+++ CoreBundle/BNCCoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BNC\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BNCCoreBundle extends Bundle
+{
+}
---
+++ CoreBundle/Controller/InitController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace BNC\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Unique Controllers necessary for this template.
+ * Class InitController
+ * @package BNC\CoreBundle\Controller
+ */
+class InitController extends Controller
+{
+
+    public function indexAction(Request $request, $controller, $options = [])
+    {
+
+    }
+}
