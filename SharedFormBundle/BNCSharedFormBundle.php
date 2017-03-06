d7831249905f2b219d5b22b375d70208d4fe7c53
chadwickmeyer <chadwick@gutensite.com>
2017-03-06 11:07:14 -0800

    Initial Commit

---
+++ SharedFormBundle/BNCSharedFormBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BNC\SharedFormBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BNCSharedFormBundle extends Bundle
+{
+}
---
+++ SharedFormBundle/Controller/InitController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace BNC\SharedFormBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Unique Controllers necessary for this template.
+ * Class InitController
+ * @package BNC\SharedFormBundle\Controller
+ */
+class InitController extends Controller
+{
+
+    public function indexAction(Request $request, $controller, $options = [])
+    {
+
+    }
+}
