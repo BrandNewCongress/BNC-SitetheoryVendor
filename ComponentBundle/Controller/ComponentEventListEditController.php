b21b30057ebfa5140440be5962a13d130a24ea70
chadwickmeyer <chadwick@gutensite.com>
2017-04-27 17:59:04 -0700

    adding custom edit page

---
+++ ComponentBundle/Controller/ComponentEventListEditController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BNC\ComponentBundle\Controller;
+
+use Sitetheory\CoreBundle\Controller\Content\ContentEditController;
+
+/**
+ * Extends the base controller for editing content.
+ *
+ * Class ComponentEventListEditController
+ * @package BNC\ComponentBundle\Controller
+ */
+class ComponentEventListEditController extends ContentEditController
+{
+    //protected $optionsInstance = [];
+}
